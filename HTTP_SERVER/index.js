57b0bf88a32331921b3deb5a3c61f5a8f32df5af
vermapiyush823 <vermapiyush823@gmail.com>
2024-02-28 23:05:06 +0530

    commit

---
+++ HTTP_SERVER/index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require('fs');
+const myServer = http.createServer((req,res)=>{
+    const log = `${Date.now()} ${req.url} New Req Reciebved\n`
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch (req.url) {
+            case '/':
+                res.end('home page');
+                break;
+            case '/about':
+                res.end('about page');
+                break;
+            default:
+                res.end('error 404!');
+                break;
+        }
+    })
+});
+
+
+myServer.listen(3000,()=>console.log("Server started"));
