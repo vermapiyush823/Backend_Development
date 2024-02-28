ae58d987d8bea190ba7a211dabec17a441c7bbd4
vermapiyush823 <vermapiyush823@gmail.com>
2024-02-28 19:55:35 +0530

    commit

---
+++ nodeJs/file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+// Synchronous
+// fs.writeFileSync("./test.txt","Hello sync");
+
+// Asynchronous
+// fs.writeFile("./test.text","Hello async",(err)=>{});
+
+// Synchronous
+// const res =  fs.readFileSync("./demo.text","utf-8");
+// console.log(res);
+
+
+// Asynchronous
+// fs.readFile("./demo.text","utf-8",(err,res)=>{
+//     if(err){
+//         console.log(`${err}`);
+//     }
+//     else{
+//         console.log(res)
+//     }
+// })
+
+// Synchronous
+// fs.appendFileSync("./text.txt",`${Date.now()} Hey There\n`);
+
+
+// Copy file
+// fs.copyFileSync("./text.txt","./copy.txt");
+
+// Delete File
+// fs.unlinkSync("./copy.txt");
+
+
+// Get details of File
+console.log(fs.statSync("./text.txt"));
