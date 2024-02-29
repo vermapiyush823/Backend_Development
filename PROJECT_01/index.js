17ede177f5ddc55dcf79ae940f48026747b28592
vermapiyush823 <vermapiyush823@gmail.com>
2024-02-29 19:14:28 +0530

    commit

--- PROJECT_01/index.js
+++ PROJECT_01/index.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const users = require('./MOCK_DATA.json')
 const PORT = 3000;
 const app = express();
-
+const url = require('url');
+// app.get('/users',(req,res)=>{
+//     const html = `
+//     <ul>
+//         ${
+//             users.map((user)=>`<li>${user.first_name}</li>`).join('')
+//         }
+//     </ul>
+//     `;
+//     res.send(html);
+// })
 
 // Routes
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+})
 
+app.get('/api/users/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id===id);
+    return res.json(user)
+})
 
+app.post('/api/users',(req,res)=>{
+    // Add new user
+    return res.json({status:'pending'});
+})
+app.patch('/api/users/:id',(req,res)=>{
+    // Update user details
+    return res.json({status:'pending'});
+})
+app.post('/api/users/:id',(req,res)=>{
+    // Delete user 
+    return res.json({status:'pending'});
+})
 
 
 app.listen(PORT,()=>{console.log(`Server started on port : ${PORT}`)})
